e15caa1b90cb16d696fbc8c7340677c98892cefd
Khateeb <khateeb321@gmail.com>
2019-02-14 14:51:55 +0100

    Simpler Module Export

--- stuff.js
+++ stuff.js
@@ -1,13 +1,9 @@
-var counter = function(arr){
+module.exports.counter = function(arr){
     return "Length: " + arr.length;
 };
 
-var adder = function(a, b){
+module.exports.adder = function(a, b){
     return `Sum is ${a+b}`;
 };
 
-var pi = 3.14;
-
-module.exports.counter = counter;
-module.exports.pi = pi;
-module.exports.adder = adder;
+module.exports.pi = 3.14;
