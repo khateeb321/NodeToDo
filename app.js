c03b46671bad6227daa70d57c6e994fb2fe956db
Khateeb <khateeb321@gmail.com>
2019-02-14 15:09:33 +0100

    util, inherits, custom events, built-in modules

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var events = require('events');
+var util = require('util');
 
-var myEmitter = new events.EventEmitter();
+var Person = function(name){
+    this.name = name;
+};
 
-myEmitter.on('someEvent', function(msg){
-    console.log(msg);
+util.inherits(Person, events.EventEmitter); //Inherited functionality 
+
+var p1 = new Person('P1');
+var p2 = new Person('P2');
+var p3 = new Person('P3');
+
+var people = [p1, p2, p3];
+
+people.forEach(function(person){
+    person.on('speak', function(msg){ // attaching cutom events
+        console.log(person.name + " said: " + msg);
+    });
 });
 
-myEmitter.emit('someEvent', "log to console");
+p1.emit('speak', 'I am P1');
+p3.emit('speak', 'I am P3');
