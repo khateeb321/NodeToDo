d09b8f7c790dc4b183fa9db343757ebd31cef160
Khateeb <khateeb321@gmail.com>
2019-02-14 15:22:23 +0100

    read and write file using fs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NodeToDo
+
+Read Me
--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function(name){
-    this.name = name;
-};
+var readMe = fs.readFileSync('README.md', 'utf8');
 
-util.inherits(Person, events.EventEmitter); //Inherited functionality 
-
-var p1 = new Person('P1');
-var p2 = new Person('P2');
-var p3 = new Person('P3');
-
-var people = [p1, p2, p3];
-
-people.forEach(function(person){
-    person.on('speak', function(msg){ // attaching cutom events
-        console.log(person.name + " said: " + msg);
-    });
-});
-
-p1.emit('speak', 'I am P1');
-p3.emit('speak', 'I am P3');
+fs.writeFileSync('newreadme.txt', readMe);
